4869282febf8c0491a96cb99136e15257ae565da
Clebson Cardoso <clebsondm@gmail.com>
2022-05-01 22:28:11 -0300

    Comments example.

--- chapter_3/4_comments/comments/src/main.rs
+++ chapter_3/4_comments/comments/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    println!("Comments in rust are all lines startgin with double /. Check the code source.");
+    println!("Comments in rust are all lines startgin with double /. Check the source code bellow.");
     // This is a comment.
     // This is another comment.
-
+    // There is also the documentation comments that will be explained in chapter 14.
 }
