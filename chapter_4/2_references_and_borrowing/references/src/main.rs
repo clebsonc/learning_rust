892ddf3bdfd0dbc5be4e75a25318fa381abcdb9e
Clebson Cardoso <clebsondm@gmail.com>
2022-05-08 15:11:04 -0300

    Learning slices.

--- chapter_4/2_references_and_borrowing/references/src/main.rs
+++ chapter_4/2_references_and_borrowing/references/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let size = receive_reference(&s);
     println!("The string `{}` has size of `{}`.", s, size);
 
-    modify_reference(&mut s);  // we pass a mutable reference for variable s
+    modify_reference(&mut s); // we pass a mutable reference for variable s
     println!("The string is now changed: `{}`", s);
 
     // rust can prevent data races at compile time, since it is not possible to have more than one
@@ -15,16 +15,15 @@ fn main() {
     // println!("{}, {}", r1, r2);
 }
 
-fn receive_reference(value: &String) -> usize {  // value is a reference to variable s fro mthe main function.
+fn receive_reference(value: &String) -> usize {
+    // value is a reference to variable s fro mthe main function.
     value.len()
     // when this function ends, nothing is dropped, since values does not have a permission to clean
     // the memory from the heap. Who does this is the main function, since the owner of this memory
     // space is the variable `s`.
 }
 
-
-fn modify_reference(word: &mut String) { // receives a mutable reference of type String
+fn modify_reference(word: &mut String) {
+    // receives a mutable reference of type String
     word.push_str(" World!");
 }
-
-
--- chapter_4/2_references_and_borrowing/slices/src/main.rs
+++ chapter_4/2_references_and_borrowing/slices/src/main.rs
@@ -1,3 +1,113 @@
+// slices allow to access a contiguos sequence of elements in a collection rather than the whole
+// collection. A slice is just a reference, so it does not have ownership.
+
+use std::str::from_utf8;
+
 fn main() {
-    println!("Hello, world!");
+    let sentence = String::from("Spring is the most beautifull season of the year!");
+    println!("\nUnderstanding characters and bytes:\n");
+    understanding_strings(&sentence);
+
+    println!("\n-------------------------------------------\n");
+    println!("The first word finishes at index {}", first_word(&sentence));
+
+    println!("\n-------------------------------------------\n");
+    println!(
+        "The first word finishes at index {}",
+        fist_word_with_enumerator(&sentence)
+    );
+
+    println!("\n-------------------------------------------\n");
+    understanding_string_slices();
+
+    println!("\n-------------------------------------------\n");
+    println!("Retrieving word as slice: {:?}", first_word_with_string_slices(&sentence));
+
+    println!("\n-------------------------------------------\n");
+    println!("Retrieving word passing a String: {:?}", first_word_that_accepts_strings_and_string_literals(&sentence));
+    let sentence2: &str = "Summer is to hot!";
+    println!("Retrieving word passing a string literal: {:?}", first_word_that_accepts_strings_and_string_literals(&sentence2));
+
+    other_slices();
+}
+
+fn understanding_strings(text: &String) {
+    println!("Sentence: `{}`", text);
+    let bytes = text.as_bytes();
+    println!("Sentence as bytes: {:?}", bytes);
+
+    // Getting the first character
+    let character = from_utf8(&bytes[0..1]).unwrap();
+    println!("Byte 0 as char: {:?}", character);
+    println!("Letter `S` as bytes: {}", b'S');
+}
+
+fn first_word(text: &String) -> usize {
+    // find the end of the first word:
+    let mut counter: usize = 0;
+    for &i in text.as_bytes() {
+        if i == b' ' {
+            return counter;
+        }
+        counter += 1;
+    }
+    counter
+}
+
+fn fist_word_with_enumerator(text: &String) -> usize {
+    for (index, &value) in text.as_bytes().iter().enumerate() {
+        if value == b' ' {
+            return index;
+        }
+    }
+    text.len()
+}
+
+
+fn understanding_string_slices() {
+    // string slices are just literal strings, this is why its type &str
+    let sentence = String::from("Hello beautifull world!");
+    let first_slice: &str = &sentence[0..4];
+    let second_slice: &str = &sentence[4..7];
+    println!("First Slice: {}", first_slice);
+    println!("First Slice: {}", second_slice);
+
+    let ommiting_starting: &str = &sentence[..5];
+    let ommiting_end: &str = &sentence[5..];
+    let ommiting_both_sides: &str = &sentence[..];
+    println!("Ommiting start: {}", ommiting_starting);
+    println!("Ommiting end: {}", ommiting_end);
+    println!("Ommiting both sides: {}", ommiting_both_sides);
+}
+
+
+fn first_word_with_string_slices(sentence: &String) -> &str { // the return type is a string slice, therefore it is immutable.
+    let bytes: &[u8] = sentence.as_bytes();
+    for (i, &value) in bytes.iter().enumerate() {
+        if value == b' ' {
+            return &sentence[..i];
+        }
+    }
+    &sentence[..]
+}
+
+
+fn first_word_that_accepts_strings_and_string_literals(sentence: &str) -> &str {
+    let bytes: &[u8] = sentence.as_bytes();
+    for (index, &value) in bytes.iter().enumerate() {
+        if value == b' ' {
+            return &sentence[..index];
+        }
+    }
+    &sentence[..]
+}
+
+
+fn other_slices() {
+    // others collections can also be sliced just as strings:
+    let values: [u8; 5] = [10, 20, 30, 40, 50];
+    let slice1: &[u8] = &values[..3];
+    let slice2: &[u8] = &values[3..];
+    println!("Slice 1: {:?}", slice1);
+    println!("Slice 2: {:?}", slice2);
 }
