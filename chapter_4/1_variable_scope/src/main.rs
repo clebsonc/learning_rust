891ec74963555fedaabb15d6569af80ec71abde0
Clebson Cardoso <clebsondm@gmail.com>
2022-05-08 13:08:09 -0300

    learning references and borrowing

--- chapter_4/1_variable_scope/src/main.rs
+++ chapter_4/1_variable_scope/src/main.rs
@@ -11,10 +11,17 @@ fn main() {
                          // time. String literals are immutable, since it is created in the stack.
         println!("{}", s);
         // when this scope finishes, the variable s no longer exists.
-    }  // here the `drop` function is callep upon the s string.
+    } // here the `drop` function is callep upon the s string.
 
     the_string_type();
     ways_variables_interact();
+    // the semantics for passing value to a function works similar to variables.
+    ownership_and_functions();
+
+    // it is possible to return more than one value from fuctions:
+    let word = String::from("triangle");
+    let (word, len) = compute_length(word);
+    println!("String: {} - size: {}", word, len);
 }
 
 fn the_string_type() {
@@ -31,19 +38,54 @@ fn ways_variables_interact() {
     // items that are created on the stack can be copied to other variables, whist items that
     // are created on the heap are shallow copied.
     let x = 5;
-    let y = x;  // here y is a copy of x, and has its owns space on the stack.
+    let y = x; // here y is a copy of x, and has its owns space on the stack.
     println!("{} - {}", x, y);
 
     let s1 = String::from("hello");
-    let s2 = s1;  // here s2 is a shallow copy from s1. This means that s1 looses its reference on
-                  // the heap for the string.
-                  // This means that the content of `s1` is moved to s2. After this s1 is invalid.
-    println!("{}",  s2);
-
+    let s2 = s1; // here s2 is a shallow copy from s1. This means that s1 looses its reference on
+                 // the heap for the string.
+                 // This means that the content of `s1` is moved to s2. After this s1 is invalid.
+    println!("{}", s2);
 
     // If we want to make a deep copy of a String to another variable, we must clone its content:
-    let s3 = s2.clone();  // this means that there is another pointer (s3) on the stack mappinglet
-                          // to other position on the heap that is different from what s2 is
-                          // pointing to.
+    let s3 = s2.clone(); // this means that there is another pointer (s3) on the stack mappinglet
+                         // to other position on the heap that is different from what s2 is
+                         // pointing to.
     println!("{}", s3);
+}
+
+fn ownership_and_functions() {
+    let s = String::from("hello"); // s comes into scope
+    takes_ownership(s); // move the ownership from `s` to `some_string` in function
+                        // takes_ownership. Since `gives` its ownership to `somestring`, when the
+                        // function is finished it is no longer valid.
+
+    // scalar types do not move ownership, since it is created on the stack.
+    let i = 50;
+    sum_value(i);
+    // It is ok to call `i` here, since it does not give ownership of its content to function `sum_value`.
+
+    // if we want to retrieve the ownership back, the function must return it.
+    let mut value = String::from("Hello");
+    value = takes_and_gives_back(value);
+    // now we can use value again:
+    println!("{}", value);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+} // when the function finishes the `drop` function is called and free the memory for `some_string`.
+
+fn sum_value(value: u8) {
+    println!("{}", value + 10);
+}
+
+fn takes_and_gives_back(mut receiving: String) -> String {
+    receiving.push_str(" World!");
+    receiving
+}
+
+fn compute_length(word: String) -> (String, usize) {
+    let length = word.len();
+    (word, length)
 }
---
+++ chapter_4/2_references_and_borrowing/references/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    // Instead of giving the ownership to a variable or a function we can pass its reference with
+    // the operator &.
+    let mut s = String::from("Hello");
+    let size = receive_reference(&s);
+    println!("The string `{}` has size of `{}`.", s, size);
+
+    modify_reference(&mut s);  // we pass a mutable reference for variable s
+    println!("The string is now changed: `{}`", s);
+
+    // rust can prevent data races at compile time, since it is not possible to have more than one
+    // mutable reference at the same time for the same data:
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+    // println!("{}, {}", r1, r2);
+}
+
+fn receive_reference(value: &String) -> usize {  // value is a reference to variable s fro mthe main function.
+    value.len()
+    // when this function ends, nothing is dropped, since values does not have a permission to clean
+    // the memory from the heap. Who does this is the main function, since the owner of this memory
+    // space is the variable `s`.
+}
+
+
+fn modify_reference(word: &mut String) { // receives a mutable reference of type String
+    word.push_str(" World!");
+}
+
+
