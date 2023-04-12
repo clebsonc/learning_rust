81d173ba0562199a0307503723bdcb1c2e06f1ec
Clebson Cardoso <clebsondm@gmail.com>
2023-04-12 01:16:51 -0300

    Practicing.

--- rustlings/exercises/modules/modules2.rs
+++ rustlings/exercises/modules/modules2.rs
@@ -7,24 +7,25 @@
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &'static str = "Cucumber";
         pub const CARROT: &'static str = "Carrot";
     }
 }
 
 fn main() {
+    use crate::delicious_snacks::{fruit, veggie};
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie
+        fruit,
+        veggie
     );
 }
--- rustlings/exercises/strings/strings3.rs
+++ rustlings/exercises/strings/strings3.rs
@@ -1,21 +1,22 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut trimmed = String::from(input.trim());
+    trimmed
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut composed = String::from(input);
+    composed.push_str(" world!");
+    composed
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut replaced = String::from(input);
+    replaced = replaced.replace("cars", "balloons");
+    replaced
 }
 
 #[cfg(test)]
