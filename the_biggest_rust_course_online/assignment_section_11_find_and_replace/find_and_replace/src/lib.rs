9ba8fe2bdc857628150bbee12eca8ae51d7692d3
Clebson Cardoso <clebsondm@gmail.com>
2024-01-24 23:15:03 -0300

    test regex and clap

--- the_biggest_rust_course_online/assignment_section_11_find_and_replace/find_and_replace/src/lib.rs
+++ the_biggest_rust_course_online/assignment_section_11_find_and_replace/find_and_replace/src/lib.rs
@@ -1,20 +1,76 @@
+use clap::Parser;
 use regex::Regex;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::Write;
+use std::path::Path;
+use text_colorizer::Colorize;
 
-#[allow(dead_code)]
-#[allow(unused_variables)]
+/// Simple program to find and replace text in file
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// Name of the input file to find text
+    #[arg(short, long)]
+    pub input: String,
+
+    /// Name of the output file with replaced data
+    #[arg(short, long)]
+    pub output: String,
+
+    /// Text that will be replaced
+    #[arg(short, long)]
+    pub find: String,
+
+    /// New text that will replace the pattern
+    #[arg(short, long)]
+    pub replace: String,
+}
+
 pub fn replace(full_text: &str, to_find: &str, to_replace: &str) -> String {
     // panic if invalid
     let re = Regex::new(to_find).unwrap();
 
+    let count_matches = re.find_iter(full_text).count();
+    println!(
+        "Number of found matches for {} is {}. They are all going to be replaced for {}",
+        to_find.red(),
+        count_matches.to_string().blue(),
+        to_replace.green(),
+    );
     let result = re.replace_all(full_text, to_replace).to_string();
-    
+
     let spaces = Regex::new(r"\s+").unwrap();
     spaces.replace_all(&result, " ").trim().to_string()
 }
 
+pub fn open_file(path: &String) -> String {
+    let p = Path::new(path);
+    let mut f = File::open(p).unwrap();
+    let mut data = String::new();
+    f.read_to_string(&mut data).unwrap();
+    data
+}
+
+pub fn save_to_file(path: &String, data: &String) {
+    let p = Path::new(path);
+    let rfc = File::create(p);
+    let mut f = match rfc {
+        Ok(f) => {
+            println!("Output file is correctly created: {}", path.blue());
+            f
+        }
+        Err(e) => panic!(
+            "The file was not created as expected: {}. Error message: {}",
+            path.blue(),
+            e
+        ),
+    };
+    f.write_all(data.as_bytes()).unwrap();
+}
 
 #[cfg(test)]
-mod test{
+mod test {
     use super::replace;
 
     #[test]
--- the_biggest_rust_course_online/assignment_section_11_find_and_replace/find_and_replace/src/main.rs
+++ the_biggest_rust_course_online/assignment_section_11_find_and_replace/find_and_replace/src/main.rs
@@ -1,35 +1,11 @@
-#[allow(unused_imports)]
-use std::env::args;
-
-#[allow(unused_imports)]
-use clap::{Parser, Subcommand};
+use clap::Parser;
+use find_and_replace::*;
 
 
-/// Simple program to find and replace text in file
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the input file to find text
-    #[arg(short, long)]
-    input: String,
-
-    /// Name of the output file with replaced data
-    #[arg(short, long)]
-    output: String,
-
-    /// Text that will be replaced
-    #[arg(short, long)]
-    find: String,
-
-    /// New text that will replace the pattern
-    #[arg(short, long)]
-    replace: String,
-}
-
-use find_and_replace::replace;
-
-fn main() {
+fn main(){
     let args = Args::parse();
-    let result = replace(&args.input, &args.find, &args.replace);
-    println!("{}", result);
+    let data = open_file(&args.input);
+
+    let result = replace(&data, &args.find, &args.replace);
+    save_to_file(&args.output, &result);
 }
