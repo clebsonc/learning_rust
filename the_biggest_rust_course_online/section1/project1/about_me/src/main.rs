14faf98751a64415b87ac732d8769e62b967a43e
Clebson Cardoso Alves de Sa <clebsondm@gmail.com>
2026-05-30 19:36:42 -0300

    fixing section 1 and 2

---
+++ the_biggest_rust_course_online/section1/project1/about_me/src/main.rs
@@ -0,0 +1,9 @@
+// just compile with `cargo build`
+// run with `cargo run`
+
+fn main() {
+    println!("Hello, world!");
+    println!("Hello, Person");
+    println!("Hello, Other Person");
+    print!("Hello, world!");
+}
---
+++ the_biggest_rust_course_online/section2/project/season_game/src/main.rs
@@ -0,0 +1,23 @@
+#[allow(unused_variables)]
+const TOUCHDOWN_POINTS: u8 = 6;
+
+
+
+fn main() {
+    let season = "Summer";
+    #[allow(unused_assignments)]
+    let mut points_scored: u32 = 28;
+    points_scored = 35;
+    type 
+
+    #[allow(unused_variables)]
+    let event_time = String::from("06:00");
+    let event_time: u8 = 6;
+    println!(
+        "Season {0}\nPoints Scored {1}\nEvent time {2}\nTouch DOWN Points: {3}",
+        season, points_scored, event_time, TOUCHDOWN_POINTS
+    );
+
+    #[allow(unused_variables)]
+    let beverage: &str = "Diet Coke";
+}
